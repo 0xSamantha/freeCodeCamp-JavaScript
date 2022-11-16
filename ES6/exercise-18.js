6004b69ca29090ca3c9ab7840ae4fb72275ead7d
samcmcr <samchavoya@gmail.com>
2022-11-16 11:22:08 -0800

    class syntax for constructor func. - exercise18

---
+++ ES6/exercise-18.js
@@ -0,0 +1,20 @@
+// Use the class keyword and write a constructor to create the Vegetable class.
+
+// The Vegetable class allows you to create a vegetable object with a property name that gets passed to the constructor.
+// Example format:
+// class SpaceShuttle {
+//  constructor(targetPlanet) {
+//    this.targetPlanet = targetPlanet;
+//  }
+
+// Only change code below this line
+class Vegetable {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    }
+  // Only change code above this line
+  
+  const carrot = new Vegetable('carrot');
+  console.log(carrot.name); // Should display 'carrot'
